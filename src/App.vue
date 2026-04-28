4074586e0400fd24f0b7925dd70cc00e480ac749
anna.pavanello <annaclaramorettipavanello@gmail.com>
2026-04-28 11:58:41 -0300

    alterações no código

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,34 @@ const produtos = ref([
 { id: 8, nome: 'Coleira Azul', preco: 28, categoria: 'Acessórios' },
 { id: 9, nome: 'Guia de Passeio', preco: 40, categoria: 'Acessórios' }
 ])
+
+const preco = ref(0);
+const posicaoProduto = ref (-1);
+const alterando = ref(false);
+
+function corrigirPreco(idProduto){
+posicaoProduto.value = produtos.value.findIndex(p => p.id === idProduto);
+preco.value = produtos.value[posicaoProduto.value].preco;
+alterando.value = true;
+}
+
+function salvarPreco(){
+produtos.value[posicaoProduto.value].preco = preco.value
+alterando.value = false;
+}
 </script>
 
 <template>
   <div class="container">
     <ul>
-      <ProdutoChild v-for="produto in produtos" :key="produto.id" :id="produto.id" :nome="produto.nome" :preco="produto.preco" :categoria="produto.categoria">
+      <ProdutoChild v-for="produto in produtos" :key="produto.id" :id="produto.id" :nome="produto.nome" :preco="produto.preco" :categoria="produto.categoria" @corrigirpreco="corrigirPreco">
 
       </ProdutoChild>
     </ul>
-    <div v-show="true">
+    <div v-show="alterando">
       <label>Preço</label>
-      <input type="number" v-model="preco">
-      <ButtonChild @clique="corrigirPreco">Salvar</ButtonChild>
+      <input type="number" v-model.number="preco">
+      <ButtonChild @clique="salvarPreco">Salvar</ButtonChild>
     </div>
   </div>
 </template>
