b48ab1bcbfa19b9e018abeab717458284a02134c
karinycapanema <capanemakariny@gmail.com>
2026-05-05 11:57:12 -0300

    atualização

---
+++ src/views/AlimentosViews.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { listaProdutos } from '@/data/produtos';
+import { ref } from 'vue'
+const produtos = ref(listaProdutos);
+produtos.value = produtos.value.filter(p => p.categoria === 'Alimentos' );
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped> 
+
+</style>
