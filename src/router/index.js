f906ce33bbf536f9e1f12926dd150bcbd629c627
karinycapanema <capanemakariny@gmail.com>
2026-05-12 11:29:58 -0300

    atualização

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AlimentosViews from '../views/AlimentosViews.vue';
 import HomeView from '@/views/HomeView.vue';
+import BrinquedosViews from '@/views/BrinquedosViews.vue';
 
 const routes = [
   { path: '/', component: HomeView},
-  { path: '/alimentos', component: AlimentosViews }
+  { path: '/alimentos', component: AlimentosViews },
+  { path: '/brinquedos', component: BrinquedosViews}
 ];
 
 const router = createRouter({
